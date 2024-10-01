e4300f43b5b705b22beaa4dab361eb2190ffb018
Ma Anastasiya <80986121+MaAnastasiya@users.noreply.github.com>
2024-10-02 00:29:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,10 @@ def sample():
     # Получаем параметр 'x' из URL
     x = request.args.get('x', type=int)
 
-    # Если x не передан, возвращаем 404 Not Found
     if x is None:
-        return Response("Not Found", status=404)  # Возвращаем 404
+        response = Response(status=200)
+        response.headers['Content-Type'] = 'text/plain; charset=UTF-8'
+        return response
 
     # Выполняем задачу
     result = task(x)
